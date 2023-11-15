f466d7c8d554d68068751a43c689702c7a7cdd0a
Edward Faulkner <edward@eaf4.com>
2023-11-15 17:32:43 -0500

    Build dist/dependencies via rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,90 @@
+/* eslint-env node */
+
+import { dirname, parse, resolve } from 'node:path';
+import { existsSync } from 'node:fs';
+import { createRequire } from 'node:module';
+import { fileURLToPath } from 'node:url';
+
+const require = createRequire(import.meta.url);
+const { PackageCache } = require('@embroider/shared-internals');
+const packageCache = PackageCache.shared('ember-source', dirname(fileURLToPath(import.meta.url)));
+
+export default {
+  input: {
+    'dependencies/backburner.js': require.resolve('backburner.js/dist/es6/backburner.js'),
+    'dependencies/rsvp': require.resolve('rsvp/lib/rsvp.js'),
+    'dependencies/dag-map': require.resolve('dag-map/dag-map.js'),
+    'dependencies/router_js': require.resolve('router_js/dist/modules/index.js'),
+    'dependencies/route-recognizer': require.resolve(
+      'route-recognizer/dist/route-recognizer.es.js'
+    ),
+    ...walkGlimmerDeps([
+      '@glimmer/node',
+      '@simple-dom/document',
+      '@glimmer/manager',
+      '@glimmer/destroyable',
+      '@glimmer/owner',
+      '@glimmer/opcode-compiler',
+      '@glimmer/runtime',
+    ]),
+  },
+  output: {
+    format: 'es',
+    dir: 'dist',
+    exports: 'named',
+  },
+};
+
+
+function walkGlimmerDeps(packageNames) {
+  let seen = new Set();
+  let entrypoints = {};
+  let queue = packageNames.map((name) => findFromProject(name));
+  let pkg;
+
+  while ((pkg = queue.pop()) !== undefined) {
+    if (seen.has(pkg)) {
+      continue;
+    }
+    seen.add(pkg);
+
+    if (!pkg.name.startsWith('@glimmer/') && !pkg.name.startsWith('@simple-dom/')) {
+      continue;
+    }
+
+    let pkgModule = entrypoint(pkg, 'module');
+
+    if (pkgModule && existsSync(pkgModule.path)) {
+      entrypoints[`dependencies/${pkg.name}`] = pkgModule.path;
+    }
+
+    let dependencies = pkg.dependencies;
+    if (dependencies) {
+      queue.push(...dependencies);
+    }
+  }
+
+  return entrypoints;
+}
+
+function findFromProject(...names) {
+  let current = packageCache.get(packageCache.appRoot);
+  for (let name of names) {
+    current = packageCache.resolve(name, current);
+  }
+  return current;
+}
+
+function entrypoint(pkg, which) {
+  let module = pkg.packageJSON[which];
+  if (!module) {
+    return;
+  }
+  let resolved = resolve(pkg.root, module);
+  let { dir, base } = parse(resolved);
+  return {
+    dir,
+    base,
+    path: resolved,
+  };
+}
